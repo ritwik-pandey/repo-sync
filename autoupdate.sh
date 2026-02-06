8c93af8dfd8a17d45845919a14af29383af59f44
Ritwik Pandey <ritwikmni@gmail.com>
2026-02-07 02:25:22 +0530

    Add cli update commands

--- autoupdate.sh
+++ autoupdate.sh
@@ -2,13 +2,13 @@
 
 
 VERSION="0.1.0"
-SCRIPT_NAME="scraper-cli"
+SCRIPT_NAME="autoupdate"
 
 show_commands(){
 	echo "Testing"
 }
 
-while [[ "$1" != " " ]]; do
+while [[ "$#" -gt 0 ]]; do
 	case $1 in 
 		-v|--version )
 			echo "$SCRIPT_NAME version: $VERSION"
@@ -18,12 +18,37 @@ while [[ "$1" != " " ]]; do
 			show_commands
 			exit 0
 			;;
+		--check-update)
+			ver=$(curl -s https://raw.githubusercontent.com/ritwik-pandey/repo-sync/main/version.txt)
+			if [[ "$(printf '%s\n' "$VERSION" "$ver" | sort -V | tail -n1)" != "$VERSION" ]]; then
+    				echo "Update available!"
+			else
+    				echo "Already on latest version"
+			fi
+			exit 0
+			;;
+		--self-update)
+			Script_path="$(realpath "$0")"
+			Script_url="https://raw.githubusercontent.com/ritwik-pandey/repo-sync/main/autoupdate.sh"
+			curl -fsSL "$Script_url" -o /tmp/autoupdate.new
+			[ -s /tmp/autoupdate.new ] || {
+				echo "download failed"
+				exit 1
+			}
+			chmod +x /tmp/autoupdate.new
+			mv /tmp/autoupdate.new "$Script_path"
+			echo "Success!"
+			exit 0
+			;;
 		*)
 			shift
 			;;
 	esac
 	shift
 done
+
+
+
 
 
 
